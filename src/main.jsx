4d2017ba82673cb08f4fade1a9f81a8dd91a8034
LucasVilarindo <lucasvilarindo@outlook.com>
2024-05-22 22:04:51 -0300

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import RelogioDigital from './RelogioDigital.jsx'
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RelogioDigital />
+  </React.StrictMode>,
+)
